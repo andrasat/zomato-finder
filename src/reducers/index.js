5f558297e893c8ad78436b6bdd7a11dd2f3ab5fb
andrasat <andra.satria1@gmail.com>
2017-05-06 23:25:42 +0700

    added reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux'
 
+import { restaurantReducer } from './restaurantReducer'
+import { userReducer } from './userReducer'
+import { loginReducer } from './loginReducer'
+
 export default combineReducers({
-  
+  restaurants: restaurantReducer,
+  loginData: loginReducer,
+  userData: userReducer
 })
--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,8 +1,22 @@
+import { AsyncStorage } from 'react-native'
+
 import * as types from '../constants'
+
+const loggedIn = (state, payload) => {
+  try {
+    AsyncStorage.multiSet([
+      ['token': payload.token],
+      ['id': payload.id]
+    ])
+    return payload
+  } catch(error) {
+    return error
+  }
+}
 
 export const loginReducer = (state = null, action) => {
   switch(action.type) {
-    case types.LOGIN_USER_SUCCESS : return action.payload
+    case types.LOGIN_USER_SUCCESS : return loggedIn(state, action.payload)
     case types.LOGIN_USER_FAIL : return action.error
     default: return state
   }
